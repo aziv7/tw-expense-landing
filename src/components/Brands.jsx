ea53190233eb5be37feda15aa62ec79fefd0a671
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-01 18:34:14 +0100

    brands component

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { overview } from '../data';
 
 const Brands = () => {
-  return <div>Brands</div>;
+  const { brands } = overview;
+  return (
+    <section className='py-9 '>
+      <div className=' container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6'>
+        {brands.map((brand, i) => (
+          <div data-aos='fade-up' data-aos-delay={brand?.delay} key={i}>
+            <img src={brand?.image} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Brands;
