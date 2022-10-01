a6e1f5cded89621679b54293b53ced8d1a3b776e
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-01 16:18:48 +0100

    components

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+export { default as Header } from './Header';
+export { default as Hero } from './Hero';
+
+export { default as Overview } from './Overview';
+
+export { default as Brands } from './Brands';
+export { default as Feature1 } from './Feature1';
+export { default as Feature2 } from './Feature2';
+export { default as Feature3 } from './Feature3';
+export { default as Product } from './Product';
+
+export { default as Pricing } from './Pricing';
+export { default as Testimonials } from './Testimonials';
+export { default as Cta } from './Cta';
+
+export { default as Footer } from './Footer';
