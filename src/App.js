f8fe6cd1ab0e70bf69ea6a56ee9a1488ff6b6d1b
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-01 16:23:37 +0100

    tw css classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import {
+  Brands,
+  Cta,
+  Feature1,
+  Feature2,
+  Feature3,
+  Footer,
+  Header,
+  Hero,
+  Overview,
+  Pricing,
+  Product,
+  Testimonials,
+} from './components';
+
+import Aos from 'aos';
+
 import logo from './logo.svg';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className=''>
+      <Header />
+      <Hero />
+      <Overview />
+      <Brands />
+
+      <Feature1 />
+
+      <Feature2 />
+      <Feature3 />
+      <Product />
+      <Pricing />
+      <Testimonials />
+      <Cta />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
