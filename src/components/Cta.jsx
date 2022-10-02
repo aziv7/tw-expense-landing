fb7bdeac9f8d78ed2c185cd5be981099e5d4e224
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-02 21:49:28 +0100

    CTA section

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
-
+import { cta } from '../data';
+import { BsChevronCompactDown } from 'react-icons/bs';
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <section className='section bg-cta bg-cover bg-left-top'>
+      <div className='max-w-[1340px] mx-auto px-[25px]'>
+        <div className='max-w-[920px] mx-auto text-center'>
+          <h2
+            className='h2 text-white mb-6 '
+            data-aos='fade-up'
+            data-aos-delay='200'>
+            {cta?.title}{' '}
+          </h2>
+
+          <p
+            data-aos='fade-up'
+            data-aos-delay='300'
+            className='text-2xl lg:text-4xl text-white '>
+            {' '}
+            {cta?.subtitle}{' '}
+          </p>
+        </div>
+        <div className='flex justify-between'>
+          <img
+            data-aos='zoom-out-right'
+            data-aos-delay='700'
+            className='hidden xl:flex'
+            src={cta?.img1}
+            alt=''
+          />
+          <button
+            data-aos='fade-up'
+            data-aos-delay='400'
+            className='btn btn-md btn-white mt-[40px] lg:text-[22px] gap-x-[10px] mx-auto  '>
+            {cta?.btnText} <BsChevronCompactDown />{' '}
+          </button>
+          <img
+            data-aos='zoom-out-left'
+            data-aos-delay='700'
+            className='hidden xl:flex'
+            src={cta?.img2}
+            alt=''
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Cta;
