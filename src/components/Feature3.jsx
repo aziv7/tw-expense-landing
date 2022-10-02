cefd19e8390e481419f9ded32bbd67e754bda17c
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-02 16:52:49 +0100

    Feature 3 section

--- src/components/Feature3.jsx
+++ src/components/Feature3.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { features } from '../data';
 
 const Feature3 = () => {
-  return <div>Feature3</div>;
+  const { feature3: feature } = features;
+  return (
+    <section className='bg-pink-50 section'>
+      <div className='container mx-auto '>
+        <div className='flex flex-col lg:flex-row lg:items-center lg:gap-x-[30px]'>
+          <div className='flex-1 ' data-aos='fade-right' data-aos-offset='400'>
+            <div className='pretitle'>{feature?.pretitle}</div>
+            <h2 className='title'>{feature?.title} </h2>
+            <p className='lead'> {feature?.subtitle} </p>
+            <button className=' btn-link flex items-center gap-x-3 hover:gap-x-5 transition-all'>
+              {' '}
+              {feature?.btnLink} <img src={feature?.btnIcon} alt='' />{' '}
+            </button>
+          </div>
+          <div className='flex-1 ' data-aos='fade-left' data-aos-offset='300'>
+            <img src={feature?.image} alt='' />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature3;
